e54d0aa8985defde4e7321b5bf9b0ab157b60afd
Oliver Dürr <oliver.duerr@gmail.com>
2023-04-28 15:59:57 +0200

    Update triangle_deeptrafo.r

--- triangle_deeptrafo.r
+++ triangle_deeptrafo.r
@@ -12,11 +12,13 @@ dgp <- function(n) {
   x <- rnorm(n)
   y <- x + rnorm(n)
   z <- 1.5*y + 0.42*x + rnorm(n)
-  data.frame(x = x, y = y, z = z)
+  data.frame(x = x, y = y, z=z)
 }
 
 dat <- dgp(42)
-m1 <- ColrNN(x ~ 1, data = dat) #Colr --> f_z is dlogis
+m1 <- ColrNN(x ~ 1, data = dat, order = 10) #Colr --> f_z is dlogis
+m1$model
+m1$model$input
 
 #### Calculation of the NLL
 ## It's tricky
@@ -69,9 +71,9 @@ rename = function(m, offset){
   return (m)
 } 
 
-m2 = rename(m2, '_m2')
 m1 = rename(m1, '_m1')
-m3 = rename(m1, '_m3')
+m2 = rename(m2, '_m2')
+m3 = rename(m3, '_m3')
 m1$model
 m1$model$input
 
@@ -99,10 +101,42 @@ IF (FALSE){
     logLik(m3,dat_new, convert_fun = mean)
 }
 
+
+######## ohne tf
+y_update = predict(m2, dat)
+class(m1)
+m1
+
+dat_update_1 = dat
+dat_update_1$y = y_update
+z_update = predict(m3, dat_update_1)
+dat_update_2 = dat_update_1
+dat_update_2$z = z_update   
+loss = logLik(m1, newdata = dat, convert_fun = mean) +
+  logLik(m2, newdata = dat_update_1, convert_fun = mean) + 
+  logLik(m3, newdata = dat_update_2, convert_fun = mean)
+
 inp <- list(m1$model$input, m2$model$input)#, m3$model$input)  #TODO Output
 out <- list(m1$model$output)#, m2$model$output, m3$model$output)
 big_mod <- keras_model(inp, out)
 compile(big_mod, loss = get_loss(mods))
+
+######## mit tf
+m1keras = m1$model
+bs = tf$Variable(matrix(runif(22), ncol = 11, nrow = 2), dtype='float32')
+bsz = tf$Variable(matrix(0, ncol = 11, nrow = 2), dtype='float32')
+ia = tf$Variable(matrix(c(0.3,0.3), ncol = 1, nrow = 2), dtype='float32')
+m1$model$input
+i = list(bs,bsz,bsz,ia,ia)
+m1keras(i)
+
+m1$model$input
+bs.m1 = coef(m1, which_param = 'interacting')
+
+
+library(mlt)
+
+
 
 # input represents the trafo
 # for m1 we have 5 elements for the baselin-trafo
