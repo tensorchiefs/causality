1054fc5fd9992db6203a7b46a4ca5597ee6299b6
Oliver Dürr <oliver.duerr@gmail.com>
2023-07-13 13:57:34 +0200

    TRIANGLELIN from VACA2  works (doX)

--- R/tram_dag/triangle.r
+++ R/tram_dag/triangle.r
@@ -16,7 +16,7 @@ bp = make_bernp(len_theta)
 # See Sánchez-Martin, Rateike, and Valera, “VACA.” Appendix 
 
 #Roughly Limited to [0,1] 
-dgp <- function(n) {
+dgp_scaled <- function(n) {
   N = n
   flip = sample(c(0,1), N, replace = TRUE)
   x1 = flip*rnorm(N, -2, sqrt(1.5)) + (1-flip) * rnorm(N, 1.5, 1)
@@ -28,11 +28,22 @@ dgp <- function(n) {
   return(dat.tf)
 }
 
-dat.tf <- dgp(50000)
-dat.s = as.matrix(dat.tf$numpy())
-range(dat.s)
-hist(dat.s[,1],50)
-hist(dat.s[,2],50)
+#TRIANGLELIN from VACA2 (eqs.76-78)
+#f1(u1) = u1 + 1 
+#f2(x1, u2) = 10 · x1 − u2 
+#f3(x1, x2, u3) = 0.5 · x2 + x1 + u3
+
+dgp <- function(n) {
+  x <- rnorm(n, 0, 1) + 1
+  y <- 10 * x - rnorm(n, 0, 1) 
+  z <- 0.5 * y + 1 * x + rnorm(n, 0, 1) 
+  dat.s =  data.frame(x = x, y = y, z = z)
+  dat.tf = tf$constant(as.matrix(dat.s), dtype = 'float32')
+  return(dat.tf)
+} 
+
+dat.tf_u <- dgp(50000)
+dat.tf = scale_df(dat.tf_u)
 
 
 ##### Creation of deep transformation models ######
@@ -40,7 +51,6 @@ hist(dat.s[,2],50)
 thetaNN_x = make_model(len_theta, 1)
 parents_x = 0*dat.tf[,1] + 1 #set to 1
 target_x = dat.tf[,1, drop=FALSE]
-
 calc_NLL(thetaNN_x, parents_x, target_x)
 
 thetaNN_y = make_model(len_theta, 1)
@@ -73,9 +83,9 @@ if(FALSE){
       }
     }
     # Saving weights after training
-    thetaNN_l[[1]]$save_weights(paste0("R/tram_dag/triangle_model1_weights.h5"))
-    thetaNN_l[[2]]$save_weights(paste0("R/tram_dag/triangle_model2_weights.h5"))
-    thetaNN_l[[3]]$save_weights(paste0("R/tram_dag/triangle_model3_weights.h5"))
+    thetaNN_l[[1]]$save_weights(paste0("R/tram_dag/triangle_model1_weights_scaled.h5"))
+    thetaNN_l[[2]]$save_weights(paste0("R/tram_dag/triangle_model2_weights_scaled.h5"))
+    thetaNN_l[[3]]$save_weights(paste0("R/tram_dag/triangle_model3_weights_scaled.h5"))
     plot(loss, type='l')
 } else{
   thetaNN_l[[1]]$load_weights("R/tram_dag/triangle_model1_weights_logis.h5")
@@ -83,10 +93,12 @@ if(FALSE){
   thetaNN_l[[3]]$load_weights("R/tram_dag/triangle_model3_weights_logis.h5")
 }
 
+#######################
+# Below ad-hoc evaluation
 
 ### Distribution for X
 hist(dat.s[,1], freq = FALSE,100, main='X (Observed Data)')
-parents_x = 0*dat.tf[,1] + 1
+parents_x = 0*dat.tf[,1] + 1 #We use 
 
 #Evaluation via p(x) and 
 target_grid_R = seq(0,1,length.out=length(parents_x))
@@ -95,31 +107,18 @@ px = predict_p_target(thetaNN_l[[1]], parents = parents_x, target_grid = target_
 lines(target_grid_R, px, col='red') 
 
 #Evaluation via sampling
-sample_from_target(thetaNN_x, parents_x[1:3])
 x_samples = sample_from_target(thetaNN_x, parents_x)
 lines(density(x_samples$numpy()), col='green')
-hist(x_samples$numpy(), freq=FALSE,10000,xlim = c(0,1))
+#hist(x_samples$numpy(), freq=FALSE,10000,xlim = c(0,1))
 
-# Ploting h for x (given 1)
-doX =parents_x
-doX_tensor = 0*dat.tf[,1] + doX #set to doX
-target_grid_R = seq(-1,2,length.out=length(doX_tensor))
-target_grid = tf$cast(matrix(target_grid_R, ncol=1), tf$float32)
-h = predict_h(thetaNN_l[[2]], doX_tensor, target_grid)
-plot(target_grid_R,h, type='l')
-
-
+### Distribution for Y
 ### Marginal Distribution for Y
 hist(dat.s[,2], freq = FALSE,100)
 parents_y = x_samples
-#py = predict_p_target(thetaNN_l[[2]], parents = parents_y, target_grid = target_grid)
-#lines(target_grid_R, py)
 y_samples = sample_from_target(thetaNN_y, parents_y)
 lines(density(y_samples$numpy()),col='green')
 summary(y_samples$numpy())
 quantile(y_samples$numpy(), seq(0,0.1,0.005)) #Approx 1% smaler 0
-hist(y_samples$numpy(), freq=FALSE,1000,xlim=c(0,1))
-
 
 ### Marginal Distribution for Z
 hist(dat.s[,3], freq = FALSE,100)
@@ -128,37 +127,35 @@ parents_z = tf$concat(list(x_samples, y_samples), axis=1L)
 z_sample = sample_from_target(thetaNN_z, parents_z)
 z = z_sample$numpy()
 z=pmax(pmin(z, 1), 0)
-lines(density(z),col='pink')
-hist(z_sample$numpy(), 10000, freq = FALSE, xlim = c(0,1), ylim=c(0,10))
+lines(density(z),col='green')
+hist(z,100)
 
-######################
-# doX 
 
+############################### Do X ########################
 #Samples from Z give X=doX
-doX_triangle = function(doX, rep=1){
+doX_Chain = function(doX, rep=1){
   zs = NULL 
   for(i in 1:rep){
-    doX_tensor = 0*dat.tf[,1] + doX #set to doX
+    doX_tensor = tf$expand_dims(0*dat.tf[,1] + doX,1L) #set to doX
     ## X--> Y
     ys = sample_from_target(thetaNN_l[[2]], doX_tensor)
-    parents_z = matrix(c(doX_tensor$numpy(), ys$numpy()), ncol=2)
-    #parents_z = tf$concat(list(doX_tensor, ys), axis=1L)
+    parents_z = tf$concat(list(doX_tensor, ys), axis=1L)
     tmp = sample_from_target(thetaNN_l[[3]], parents_z)
-    zs = c(zs, tmp$numpy())
+    zs = matrix(c(doX_tensor$numpy(),ys$numpy(), tmp$numpy()), ncol=3)
   }
   return (zs)
 }
 
-z_do_xup = doX_triangle(0.5)
-hist(z_do_xup,1000,xlim=c(0,1))
-plot(ecdf(z_do_xup))
-summary(z_do_xup)
 
-z_do_down = doX_triangle(0.3)
-hist(z_do_down,1000, xlim=c(0,1))
-mean(z_do_xup) - mean(z_do_down)
-#median(z_do_xup) - median(z_do_down)
-(0.5 - 0.3*0.25)*0.2
+
+dat_do_xup = unscale(dat.tf_u, doX_Chain(doX=0.75))$numpy()
+dat_do_xdown = unscale(dat.tf_u, doX_Chain(doX=0.25))$numpy()
+
+x_0 = dat_do_xdown[1,1]
+x_1 = dat_do_xup[1,1]
+
+(mean(dat_do_xup[,2])  - mean(dat_do_xdown[,2])) / (x_1-x_0) #Should be 10.0
+(mean(dat_do_xup[,3])  - mean(dat_do_xdown[,3])) / (x_1-x_0) #Should be 6.0
 
 
 
